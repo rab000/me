e1b01cac250ade9d8437cd4f40b2fd7aa9b6a99a
cuizheng <cuizheng@fotoable.com>
2019-02-13 19:54:53 +0800

    注释

--- Assets/me/Scripts/objs/BaseObj.cs
+++ Assets/me/Scripts/objs/BaseObj.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// 专用于标识me中的object
+/// 只记录类型，不记录数据，也不在me外使用
+/// </summary>
 public class BaseObj : MonoBehaviour {
 
 	public string Type;
