52668f18ce6aa17cb60f36d806c1fd383539c450
rab000 <ce1985@126.com>
2019-02-17 21:25:07 +0800

    doc update

--- Assets/me/Doc/docment.cs
+++ Assets/me/Doc/docment.cs
@@ -9,3 +9,18 @@
 //    scn 中具体子文件夹的命名为  scnType_scnName,这样做的好处是直观  
 
 
+//  MeEditorHelper.OUTPUT_RES_PATH与bundleName的羁绊
+//  首先注意xml文件是产生在前者的路径下的
+//	1
+//  前者设置为OUTPUT_ROOT_PATH+"/res/me";
+//	后者设置为"map/" + mapName + "/" + fileNameWithoutExt
+//  此时可以正常打包，但是打出来的包的bundleName并不是 me/map/mapName/fileName,缺少了me的开头
+//	2
+//	如果给后者加上me,bundleName变为"me/map/" + mapName + "/" + fileNameWithoutExt
+//	那么打包位置就会变成OUTPUT_ROOT_PATH+"/res/me/me/.......";多了一级me，这时连续两个同名，unity打包就会报错
+//	3
+//	如果前者设置为OUTPUT_ROOT_PATH+"/res"
+//  后者设置为"me/map/" + mapName + "/" + fileNameWithoutExt
+//  这样也不行，因为xml文件会直接生成到OUTPUT_ROOT_PATH+"/res"下，这个路径是所有资源的根路径，不是me资源的根路径，而且名称是res，而不是me
+
+//	目前使用的方案是1，在游戏中读取xml中所有资源路径时，给所有资源手动加上"me"的开头
